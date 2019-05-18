0de51626daa1a3c19fe92a7cafd754d0be2a7258
twotwentyfour <twotwentyfour@protonmail.com>
2019-05-18 00:33:19 -0400

    completed week 2's assignment.

--- wbg370_serres_week2/game.js
+++ wbg370_serres_week2/game.js
@@ -32,13 +32,21 @@ game.bop = {
     atom.context.fillText('Score: ' + this.total, 300, 200);
   },
   with_key: function(key){
-    if (!!(game.activeMole + 1) === true && key === game.holes[game.activeMole].label){
-      this.total = this.total+1;
-      game.activeMole = -1;
-      this.bopped = true;
+    if (this.total === 20)
+    {
+        alert('You win!');
+	this.total = 0;
     }
-    else{
-      this.total = this.total-1;
+    else
+    {
+        if (!!(game.activeMole + 1) === true && key === game.holes[game.activeMole].label){
+          this.total = this.total+1;
+          game.activeMole = -1;
+          this.bopped = true;
+        }
+        else{
+          this.total = this.total-1;
+        }
     }
   }
 }
